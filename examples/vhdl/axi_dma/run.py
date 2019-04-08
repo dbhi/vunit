cd6f58eb98c137994cc0d73b55c09c2baea09320
umarcor <unai.martinezcorral@ehu.eus>
2019-04-08 06:32:43 +0200

    feat(examples/vhdl/axi_dma): use external memory model declared in C

--- examples/vhdl/axi_dma/run.py
+++ examples/vhdl/axi_dma/run.py
@@ -15,18 +15,35 @@ external memory. The AXI DMA also has a control register interface
 via AXI-lite.
 """
 
+from os import popen
 from os.path import join, dirname
 from vunit import VUnit
+import inspect
 
-ui = VUnit.from_argv()
+ui = VUnit.from_argv(external=[True, False])
 ui.add_osvvm()
 ui.add_verification_components()
 
 src_path = join(dirname(__file__), "src")
+ext_srcs = join(dirname(inspect.getfile(VUnit)), 'vhdl', 'data_types', 'src', 'external')
 
 axi_dma_lib = ui.add_library("axi_dma_lib")
 axi_dma_lib.add_source_files(join(src_path, "*.vhd"))
 axi_dma_lib.add_source_files(join(src_path, "test", "*.vhd"))
+
+c_obj = join(src_path, 'test', 'main.o')
+
+print(popen(' '.join([
+    'gcc -fPIC -rdynamic',
+    '-I', ext_srcs,
+    '-c', join(src_path, '**', 'main.c'),
+    '-o', c_obj
+])).read())
+
+ui.set_sim_option("ghdl.elab_flags", [
+    '-Wl,' + c_obj,
+    '-Wl,-Wl,--version-script=' + join(ext_srcs, 'grt.ver')
+])
 
 if __name__ == '__main__':
     ui.main()
